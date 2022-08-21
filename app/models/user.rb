d6a7439d0a1065866b4cdc3d48abb8b784d3fe9c
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-21 17:58:04 +0900

    新規登録機能 追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :age_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :age
+  belongs_to :category
 end

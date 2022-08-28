06aa16b8586761ef1fe30ef425b91bba6912d21e
rikakohira <rikako.takase88@gmail.com>
2022-08-28 17:58:13 +0900

    新規登録画面 JS修正

--- app/javascript/registration.js
+++ app/javascript/registration.js
@@ -2,49 +2,61 @@ var i = 1 ;
 var minCount = 0;
 var maxCount = 5;
 
-document.getElementById('add-btn').addEventListener('click', function addForm() {
-//フォーム生成
-var select = document.getElementById("assettable-list_0");
-var select_copy = select.cloneNode(true);
-select_copy.class = 'assettable-list_' + i;
-var input = document.getElementById("assettable-balance_0");
-var input_copy = input.cloneNode(true);
-input_copy.id = 'assettable-balance_' + i;
-input_copy.value = ''
+function addForm() {
+  //追加ボタンクリック時にイベント発火
+  const add = document.getElementById("add-btn");
+  add.addEventListener("click", () => {
+    //フォーム生成
+    const select = document.getElementById("selectbox_0");
+    const select_copy = select.cloneNode(true);
+    select_copy.id = 'selectbox_' + i;　
+    const input = document.getElementById("input-balance_0");
+    const input_copy = input.cloneNode(true);　　
+    input_copy.id = 'input-balance_' + i;　
+    input_copy.value = ''
+    //フォーム追加
+    if (i < maxCount) { 
+    const parent = document.getElementById('form-field');　
+    parent.appendChild(select_copy);
+    parent.appendChild(input_copy);
+    }
+ 
+    var button_data = document.createElement('button');
+    button_data.id = i;
+    button_data.onclick = function(){deleteBtn(this);}
+    button_data.innerHTML = '削除';
+    
+    if (i < maxCount) { 
+    const parent = document.getElementById('form-field');　
+    var input_area = document.getElementById(select_copy.id);
+    parent.appendChild(button_data);
+    }
+    
+    i++ ;
+	});
+ };
 
-  //フォーム追加
-  if (i < maxCount) { 
-  var parent = document.getElementById('form-field');
-  parent.appendChild(select_copy);
-  parent.appendChild(input_copy);
-  }
 
-//削除ボタン生成  
-var button_data = document.createElement('button');
-button_data.id = i;
-button_data.onclick = function(){deleteBtn(this);}
-button_data.innerHTML = '削除';
-  
-  //削除ボタン追加
-  var input_area = document.getElementById(select.id);
-  parent.appendChild(button_data);
-  
-  i++ ;
-}, false);
+window.addEventListener('load', addForm);
 
-function deleteBtn(target) {
-  //識別番号
+
+//削除ボタンクリック時に実行される
+window.deleteBtn = function(target){
   var target_id = target.id;
   var parent = document.getElementById('form-field');
-  var select_id = document.getElementById('assettable-list_' + target_id);
-  var input_id = document.getElementById('assettable-balance_' + target_id);
-  var deleteBtn_id = document.getElementById(target_id);
-    
-    //フォーム削除
-    if (target_id > minCount){
-    parent.removeChild(select_id);
-    parent.removeChild(input_id);
-    parent.removeChild(deleteBtn_id);	
-    }  
+  var slt_id = document.getElementById('selectbox_' + target_id);
+  var ipt_id = document.getElementById('input-balance_' + target_id);
+  var tgt_id = document.getElementById(target_id);
+   if (target_id > minCount){
+  parent.removeChild(slt_id);
+  parent.removeChild(ipt_id);
+  parent.removeChild(tgt_id);	
+  }  
 }
+
+    
+
+
+
+
   

f1e258823f9a44b9e42793c8b3db674bee651e67
rikakohira <rikako.takase88@gmail.com>
2022-09-01 10:51:55 +0900

    JS コメントアウト

--- app/javascript/registration.js
+++ app/javascript/registration.js
@@ -1,56 +1,73 @@
-var i = 1 ;
-const minCount = 0;
-const maxCount = 5;
+// var i = 1 ;
+// const minCount = 0;
+// const maxCount = 5;
 
-//追加ボタン
-function addForm() {
-  const add = document.getElementById("add-btn");
-  add.addEventListener("click", () => {
-    //フォーム生成
-    const select_div = document.getElementById("selectbox_0");
-    const select_div_copy = select_div.cloneNode(true);
-    //子要素１つぬきだしてくる
-    const select = select_div_copy.childNodes[1];
-    select_div_copy.id = 'selectbox_' + i;
-    select.name = 'selectbox_' + i;
-    const input = document.getElementById("input-balance_0");
-    const input_copy = input.cloneNode(true);
-    input_copy.id = 'input-balance_' + i;
-    input_copy.name = 'input_balance_' + i;
-    input_copy.value = ''
-    if (i < maxCount) { 
-    const parent = document.getElementById('form-field');
-    parent.appendChild(select_div_copy);
-    parent.appendChild(input_copy);
-    }
+// //追加ボタン
+// function addForm() {
+//   const add = document.getElementById("add-btn");
+//   add.addEventListener("click", () => {
+//     //フォーム生成
+//     const select_div = document.getElementById("selectbox_0");
+//     const select_div_copy = select_div.cloneNode(true);
+//     //子要素１つぬきだしてくる
+//     const select = select_div_copy.childNodes[1];
+//     select_div_copy.id = 'selectbox_' + i;
+//     select.name = 'selectbox_' + i;
+//     const input = document.getElementById("input-balance_0");
+//     const input_copy = input.cloneNode(true);
+//     input_copy.id = 'input-balance_' + i;
+//     input_copy.name = 'input_balance_' + i;
+//     input_copy.value = ''
+//     if (i < maxCount) { 
+//     const parent = document.getElementById('form-field');
+//     parent.appendChild(select_div_copy);
+//     parent.appendChild(input_copy);
+//     }
     
-    //削除ボタン生成
-    const button_data = document.createElement('button');
-    button_data.id = i;
-    button_data.className = 'btn btn-secondary btn-sm';
-    button_data.onclick = function(){deleteBtn(this);}
-    button_data.innerHTML = '削除';
-    if (i < maxCount) { 
-    const parent = document.getElementById('form-field');
-    var input_area = document.getElementById(select_div_copy.id);
-    parent.appendChild(button_data);
-    }
-    i++ ;
- });
- };
-window.addEventListener('load', addForm);
+//     //削除ボタン生成
+//     const button_data = document.createElement('button');
+//     button_data.id = i;
+//     button_data.className = 'btn btn-secondary btn-sm';
+//     button_data.onclick = function(){deleteBtn(this);}
+//     button_data.innerHTML = '削除';
+//     if (i < maxCount) { 
+//     const parent = document.getElementById('form-field');
+//     var input_area = document.getElementById(select_div_copy.id);
+//     parent.appendChild(button_data);
+//     }
+//     i++ ;
+//  });
+//  };
+// window.addEventListener('load', addForm);
 
 
-//削除ボタン
-window.deleteBtn = function(target){
-  const target_id = target.id;
-  const parent = document.getElementById('form-field');
-  const slt_id = document.getElementById('selectbox_' + target_id);
-  const ipt_id = document.getElementById('input-balance_' + target_id);
-  const tgt_id = document.getElementById(target_id);
-   if (target_id > minCount){
-  parent.removeChild(slt_id);
-  parent.removeChild(ipt_id);
-  parent.removeChild(tgt_id); 
-  }  
-}
+// //削除ボタン
+// window.deleteBtn = function(target){
+//   const target_id = target.id;
+//   const parent = document.getElementById('form-field');
+//   const slt_id = document.getElementById('selectbox_' + target_id);
+//   const ipt_id = document.getElementById('input-balance_' + target_id);
+//   const tgt_id = document.getElementById(target_id);
+//    if (target_id > minCount){
+//   parent.removeChild(slt_id);
+//   parent.removeChild(ipt_id);
+//   parent.removeChild(tgt_id); 
+//   }  
+// }
+
+
+
+// //勘定項目か金額が入力されていない場合はアラートを出す
+// const checkProperites = function() {
+//   debugger;
+//   let list_fields = document.getElementsByClassName("list_field_box")
+//   let balance_fields = document.getElementsByClassName("balance_field_box")
+//   for(i=0; i < list_fields.length; i++) {
+//     target_list_field = list_fields[i]
+//     target_balance_field = balance_fields[i]
+//     if ((target_list_field.childNodes[1].options.selectedIndex == 0 && target_balance_field.childNodes[1].value) || (target_list_field.childNodes[1].options.selectedIndex >= 1 && target_balance_field.childNodes[1].value == "")) {
+//       alert(`${i}行の勘定項目か金額が入力されていません。`)
+//       return false
+//     }
+//   }
+// }

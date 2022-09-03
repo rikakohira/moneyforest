fa835be5be411b4150ded78198b9acca9df146e5
rikakohira <rikako.takase88@gmail.com>
2022-09-03 16:54:45 +0900

    books#new, create 設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,27 @@ class BooksController < ApplicationController
 
   def index
   end
+
+  def new
+    @debit_credit = DebitCredit.new
+  end
+
+  def create
+    @debit_credit = DebitCredit.new(debit_credit_params)
+    if @debit_amount.valid?
+      @debit_credit.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def debit_credit_params
+    params.require(:debit_credit).permit(:date, :debit_amount, :d_list_id, :credit_amount, :c_list_id)
+  end
+
+
 end
+

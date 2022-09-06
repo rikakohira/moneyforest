7999b89827a18f698fdfbac74badabce9e4c9a61
rikakohira <rikako.takase88@gmail.com>
2022-09-06 14:08:56 +0900

    ajax　追加

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,5 +1,19 @@
-$(function() {
-  $("#seach-btn").on("click", function() {
-    $("p").hide();
+$(function () {
+  $('#search-btn').on('click', function () {
+    var selectYear = $('#q_date_during_month_1i').val();
+    var selectMonth = $('#q_date_during_month_2i').val();
+
+    $.ajax({
+    type: 'GET', // リクエストのタイプ
+    url: '/books/search', // リクエストを送信するURL
+    data:  { date_during_month: selectYear, date_during_month: selectMonth }, // サーバーに送信するデータ
+    dataType: 'json' // サーバーから返却される型
+    })
+    .done(function (data) {
+      console.log(data); // dataを確認する
+    })
+
+
+  });
 });
-});
+

bba4dde262cc36d8f54cde108070b3d6c3022030
rikakohira <rikako.takase88@gmail.com>
2022-09-09 18:22:27 +0900

    updateアクション　定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,16 @@ class UsersController < ApplicationController
   end
 
   def update
-
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:nickname, :email, :age_id, :category_id)
+    end
 end

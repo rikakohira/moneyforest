40ec0bccaf0864670a81c7862c55e3db408d138b
rikakohira <rikako.takase88@gmail.com>
2022-09-10 18:31:55 +0900

    コード　修正

--- app/controllers/books/searches_controller.rb
+++ app/controllers/books/searches_controller.rb
@@ -2,8 +2,8 @@ class Books::SearchesController < ApplicationController
   def index
     date = params[:selectYear] + '-0' + params[:selectMonth] + '-01'
     @books = Debit.joins(:list, :credit, :user).select('credits.credit_amount', 'debits.debit_amount', 'debits.memo',
-                                                       'lists.code_name', 'users.id').where(debits: { date: date,
-                                                                                                      user_id: current_user.id })
+                                                      'lists.code_name', 'users.id').where(debits: { date: date,
+                                                                                            user: current_user })
 
     respond_to do |format|
       format.html { redirect_to :root }

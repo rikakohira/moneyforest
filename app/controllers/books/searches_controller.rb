7b9466bd0f6c181d2226e7c6c1e7900b3e06a398
rikakohira <rikako.takase88@gmail.com>
2022-09-11 15:00:25 +0900

    修正

--- app/controllers/books/searches_controller.rb
+++ app/controllers/books/searches_controller.rb
@@ -1,9 +1,9 @@
 class Books::SearchesController < ApplicationController
   def index
     date = params[:selectYear] + '-0' + params[:selectMonth] + '-01'
-    @books = Debit.joins(:list, :credit, :user).select('credits.credit_amount', 'debits.debit_amount', 'debits.memo',
-                                                      'lists.code_name', 'users.id').where(debits: { date: date,
-                                                                                            user: current_user })
+    @books = Debit.joins(:list, :credit, :user).select('debits.debit_amount', 'debits.memo', 'lists.code_name', 'credits.credit_amount',
+                                                       'users.id').where(debits: { date: date,
+                                                                                   user: current_user })
 
     respond_to do |format|
       format.html { redirect_to :root }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit(:nickname, :email, :age_id, :category_id)
-    end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :age_id, :category_id)
+  end
 end

9ac219c1b75571a2846c5dc76781216b0b59ad96
rikakohira <rikako.takase88@gmail.com>
2022-09-14 19:22:05 +0900

    indexアクション　定義

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,12 @@
 class ListsController < ApplicationController
+
+  def index
+    @asset = List.where(code: 100...200)
+    @debt = List.where(code: 200...300)
+    @net_asset = List.where(code: 300)
+    @income = List.where(code: 400...500)
+    @expence = List.where(code: 500...600)
+    @profit = List.where(code: 600)
+  end
+
 end

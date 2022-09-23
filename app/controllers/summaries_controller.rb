7d5945144a9bb47fa7972679ec5d4f6718162125
rikakohira <rikako.takase88@gmail.com>
2022-09-23 21:39:26 +0900

    コミット

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,2 +1,12 @@
 class SummariesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @asset = Debit.joins(:list, :credit).select('debits.*', 'lists.code_name',
+                                                'credits.credit_amount').where(debits: { date: date, user: current_user })
+    # @debt = 
+    # @income = 
+    # @expence = 
+  end
+
 end

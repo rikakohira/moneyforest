5f7f27c6a45df3c733462e580ec2332548c512a7
rikakohira <rikako.takase88@gmail.com>
2022-09-24 14:20:12 +0900

    修正

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -2,11 +2,9 @@ class SummariesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @asset = Debit.joins(:list, :credit).select('debits.*', 'lists.code_name',
-                                                'credits.credit_amount').where(debits: { date: date, user: current_user })
-    # @debt = 
-    # @income = 
-    # @expence = 
+    # @asset = 
+    # @debt =
+    # @income =
+    # @expence =
   end
-
 end

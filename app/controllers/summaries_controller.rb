17637a5c8342d6743939749fe742bec46594a353
rikakohira <rikako.takase88@gmail.com>
2022-09-27 22:05:44 +0900

    コメントアウト

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -2,8 +2,8 @@ class SummariesController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @assettables = Assettables.all
-    @debttables = Debttables.all
+    # @assettables = Assettables.all
+    # @debttables = Debttables.all
 
     # @asset = 
     # @debt =
